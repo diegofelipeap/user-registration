190660d5172dc05b4e43fce845103a35ac06cea0
diegofelipeap <diegospd22@gmail.com>
2024-02-06 16:25:49 -0300

    Criando primeiros componentes e projeto com React e integração com Node.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+/* criando meu primeiro componente react!
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+-Toda aplicação que a gente usar o React, temos que dar um "import React from 'react'"!
+
+*/
+
+//JSX
+
+import React from 'react'
+import { Container, H1, Image, ContainerItens, InputLabel, Input, Button } from "./style";
+import People from './assets/peoples.svg'
+
+const App = () => {
+
+  return (<Container>
+    <Image />
+    <ContainerItens>
+
+      <H1>Olá!</H1>
+
+      <InputLabel>Nome</InputLabel>
+      <Input placeholder='Nome'/>
+      <InputLabel>Idade</InputLabel>
+      <Input placeholder='Idade'/>
+
+      <Button>Cadastrar</Button>
+
+    </ContainerItens>
+  </Container>)
+
 }
 
-export default App;
+export default App // isto porque no index.js estamos importando este componente, para podermos renderizar
